92912af717dec2dde87bac3561c14d7cae3c24ba
robertvillazana <robdiggityd@gmail.com>
2019-02-14 13:14:24 -0600

    added files

---
+++ keys.py
@@ -0,0 +1,8 @@
+# keys_format.py shares the same format as keys.py.
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# keys there.
+CONSUMER_KEY = 'TgSlZNSnEJ4lM2g1Z5jB70dab'
+CONSUMER_SECRET = '6XxyVx9EGFU6TOSeV5oqP5bPEJPk5mcC92ROe379CgkBgChwBv'
+ACCESS_KEY = '1093204139637653505-nmb4Lg3M2mbaonkm1jat8RXEEFx1ZK'
+ACCESS_SECRET = 'qC9vK5T17swlsvRDblsOvfi6AKvB50y7R5tWZ7cIz583d'
